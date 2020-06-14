bdaa38e108064e521bec19f782acd332ba689f4c
aenstan <54591257+aenstan@users.noreply.github.com>
2020-06-14 10:34:00 +0800

    Update github.js

--- js/github.js
+++ js/github.js
@@ -14,7 +14,7 @@
  * 📌 如果希望监控Release，请切换至Release界面，直接复制URL填入；
  */
 
-const token = "784a03feb07989d3339dfa41c7eb41777436cbfa";
+const token = "88bf5ebe7d930fe5af1b1ce5789223b2ad40f11a";
 
 const repository = [
   {
