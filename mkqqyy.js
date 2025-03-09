f3141dccbd97523fdb88089a51c2e256a5b2e5ed
aenstan <54591257+aenstan@users.noreply.github.com>
2025-03-09 17:49:15 +0800

    Update mkqqyy.js

--- mkqqyy.js
+++ mkqqyy.js
@@ -5,6 +5,11 @@
 //修改本配置显示文本和封面链接
 //然后点歌
 
+[rewrite_local]
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/axs66/qx/main/mkqqyy.js
+
+[mitm] 
+hostname = api.dragonlongzhu.cn
 
 let body = $response.body;
 if (body) {
